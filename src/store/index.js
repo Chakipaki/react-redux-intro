887c2684a692604032ab1bf7d0ac7484547dbb4c
Chakipaki <belyaeve2@icloud.com>
2023-05-04 22:12:53 +0200

    Refactoring and create store

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from 'redux';
+
+// Changes Store Data
+const counterReducer = (
+    state = { count: 0 },
+    action
+) => {
+    switch (action.type) {
+        case 'increment':
+            return {
+                counter: state.counter + 1
+            }
+        case 'decrement':
+            return {
+                counter: state.counter - 1
+            }
+        default:
+            return state;
+    }
+}
+
+// Create Redux Store
+const store = createStore(counterReducer);
+
+export default store;
